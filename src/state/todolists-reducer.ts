cd17c47f67b5692ebf827965fc3317ce6734babc
Dude343 <dude343@tut.by>
2023-08-17 15:06:55 +0300

    tests+reducer

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,16 +1,81 @@
-import { TodolistType } from "../App";
-import { ActionType } from "./user-reducer";
+import { title } from "process";
+import { FilterValuesType, TodolistType } from "../App";
+import { v1 } from "uuid";
+
+
+export type RemoveTodolistActionType = {
+    type: "REMOVE-TODOLIST",
+    id: string
+}
+
+export type AddTodolistActionType = {
+    type: "ADD-TODOLIST",
+    title: string
+}
+
+export type ChangeTodolistTitleActionType = {
+    type: "CHANGE-TODOLIST-TITLE",
+    id: string
+    title: string
+}
+
+export type ChangeTodolistFilterActionType = {
+    type: "CHANGE-TODOLIST-FILTER",
+    id: string
+    filter: FilterValuesType
+}
+
+export const RemoveTodolistAC = (todolistId: string): RemoveTodolistActionType => {
+    return { type: 'REMOVE-TODOLIST', id: todolistId }
+}
+
+export const AddTodolistAC = (title: string): AddTodolistActionType => {
+    return { type: 'ADD-TODOLIST', title: title }
+}
+
+export const ChangeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleActionType => {
+    return { type: 'CHANGE-TODOLIST-TITLE', id: id, title: title }
+}
+
+export const ChangeTodolistFilterAC = (id: string, filter: FilterValuesType): ChangeTodolistFilterActionType => {
+    return { type: 'CHANGE-TODOLIST-FILTER', id: id, filter: filter }
+}
+
+type ActionsType = RemoveTodolistActionType | AddTodolistActionType | ChangeTodolistTitleActionType | ChangeTodolistFilterActionType
 
 
 
-export const todolistsReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType): Array<TodolistType> => {
     switch (action.type) {
-        case 'REMOVE-TODOLIST'{
-            return ()...state,
 
+        case "REMOVE-TODOLIST": {
+            return state.filter((tl) => tl.id != action.id)
         }
 
+        case "ADD-TODOLIST": {
+            return [...state, {
+                id: v1(),
+                title: action.title,
+                filter: 'all'
+            }]
+        }
+
+        case "CHANGE-TODOLIST-TITLE": {
+            const todolist = state.find((tl) => tl.id === action.id);
+            if (todolist) {
+                // если нашёлся - изменим ему заголовок
+                todolist.title = action.title
+            } return [...state]
+        }
+
+        case "CHANGE-TODOLIST-FILTER": {
+            const todolist = state.find((tl) => tl.id === action.id);
+            if (todolist) {
+                // если нашёлся - изменим ему фильтр
+                todolist.filter = action.filter
+            } return [...state]
+        }
         default:
-            throw new Error('I don\'t understand this type');
+            throw new Error('I don\'t understand this type')
     }
 };
